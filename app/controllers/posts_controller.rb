f8981b92f6eeee029e1811f2415822b9fb16400b
nakamura <ynakamara04@gmail.com>
2021-01-29 00:30:02 +0900

    削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,14 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
 
   def destroy
