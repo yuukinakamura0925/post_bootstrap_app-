f78230b248f7fc75ad814937945726707db18086
nakamura <ynakamara04@gmail.com>
2021-01-28 16:57:36 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
   def index
+    
   end
 
   def show
+
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.create!(post_params)
+    redirect_to post
   end
 
   def edit
@@ -18,5 +23,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
