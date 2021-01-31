e1d0e4129725ade28d43aaea60dc8d4ab196745e
nakamura <ynakamara04@gmail.com>
2021-02-01 01:03:40 +0900

    エラーの確認用

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: "投稿しました"
     else
-      flash.now[:alert] = "投稿に失敗しました"
+      flash[:alert] = "投稿に失敗しました"
       render :new
     end 
   end
